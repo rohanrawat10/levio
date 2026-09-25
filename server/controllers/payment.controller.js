5080af2acd4fbaa61df6ab0696ccf44430829cb6
rohanrawat10 <rohanrawat10@users.noreply.github.com>
2026-09-25 15:01:10 +0530

    Payment page backend created

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,22 +1,77 @@
-import mongoose from "mongoose";
+import payments from "razorpay/dist/types/payments";
+import Payment from "../models/payment.mdel";
+import razorpay from "../services/razorpay.service";
 
-const paymentSchema = new mongoose.Schema({
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true,
-    },
-    planId:String,
-    amount:Number,
-    credits:Number,
-    razorpayOrderId:String,
-    razorpayPaymentId:String,
-    status:{
-        type:String,
-        enum:['created','paid',"failed"],
-        default:"created"
+export const createOrder = async(req,res)=>{
+    try{
+      const {planId,amount,credits} = req.body;
+      if(!amount || !credits){
+        return res.status(400).json({message:"Invalid Plan Data"})
+      }
+
+      const options = {
+        amount:amount * 100,//convert to paise
+        currency:"INR",
+        receipt:`receipt_${Date.now}`
+      }
+
+      const order = await razorpay.orders.create(options);
+      await Payment.create({
+        userId:req.userId,
+        planId,
+        amount,
+        credits,
+        razorpayOrderId:order.id,
+        status:"created"
+      })
+      res.json
     }
-},{timestamps:true})
+    catch(err){
+        res.status(500).json({message:`failed ot create Razorpay order: ${err}`})
+    }
+}
 
-const payment = mongoose.model("Payment",paymentSchema);
-export default payment;
+export const verifyPayment = async(req,res)=>{
+    try{
+        const{razorpay_order_id,
+            razorpay_payment_id,
+            razorpay_signature
+        } = req.body;
+       const body = razorpay_order_id + "|" + razorpay_payment_id;
+       const expectedSignature = crypto
+       .CreateHmac("sha256",process.env.RAZORPAY_KEY_SECRET)
+       .update(body)
+       .digest("hex");
+
+       if(expectedSignature !== razorpay_signature){
+        return res.status(400).json({message:"Invalid payment signature"})
+       }
+
+       const payment = await Payment.findOne({
+        razorpayOrderId:razorpay_order_id,
+       });
+       if(!payment){
+        return res.json({message:"Already processed"});
+       }
+
+       //update payment record
+     payment.status = "paid";
+     payment.razorpayPaymentId  = razorpay_payment_id;
+     await payment.save();
+
+     // add credits to user
+     const updateUser = await User.findByIdAndUpdate(payment.userId,{
+        $inc:{credits:payment.credits}
+     },{new:true})
+
+     res.json({
+        success:true,
+        message:"Payment verified and credits added",
+        user:updateUser,
+     })
+
+    }
+    catch(err){
+        res.status(500).json({message:`verify payment error ${err}`})
+    }
+}
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import connectDb from "./config/db.js";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import interviewRoute from "./routes/interview.route.js";
+import paymentRouter from "./routes/payment.route.js";
 const app = express();
 const server = http.createServer(app)
 const port = process.env.PORT || 4000;
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use("/api/auth",authRouter);
 app.use("/api/user",userRouter);
 app.use("/api/interview",interviewRoute);
+app.use("/api/payment",paymentRouter)
 // console.log("PORT from env:", process.env.PORT);
 server.listen(port,()=>{
     connectDb()
